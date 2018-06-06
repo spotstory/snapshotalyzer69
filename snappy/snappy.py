ccd002a84f1271bcb4b77307c72c2ef36e46406f
Jeff Nayler <jeff@joyshill.com>
2018-06-06 10:38:06 +0100

    Refactor for click

--- snappy/snappy.py
+++ snappy/snappy.py
@@ -1,8 +1,28 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='snappy')
+ec2 = session.resource('ec2')
+
+def find_key(instance, tag_name):
+    for i in instance.tags:
+        if i['Key'] == 'Project':
+            return i['Key'] + "=" + i['Value']
+
+@click.command()
+def list_snapshots():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        project_name = find_key(i, "Project")
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name,
+            project_name)))
+
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='snappy')
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    list_snapshots()
