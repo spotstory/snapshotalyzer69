ce4137df6365edbd2af382789523155a30665798
Jeff Nayler <jeff@joyshill.com>
2018-06-07 13:42:11 +0100

    Add option to list latest or all snapshots

--- snappy/snappy.py
+++ snappy/snappy.py
@@ -30,7 +30,9 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None,
     help="Only snapshots for project (tag Project:<name>)")
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True,
+    help="List all snapshots for each volume, not just the latest")
+def list_snapshots(project, list_all):
     "List snapshots"
 
     instances = filter_instances(project)
@@ -45,6 +47,8 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+
+                if s.state == 'completed' and not list_all: break
 
     return
 
